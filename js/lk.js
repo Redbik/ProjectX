bda40dc9001ea5f2d87df24585ff79c77956536b
Nisalay <sashi98@mail.ru>
2017-10-06 18:45:46 +0400

    #

--- js/lk.js
+++ js/lk.js
@@ -51,19 +51,14 @@ function sel_photos_lk() {
 var photos = [];
 function add_photos() {
     var img = $(".add-photo-form").find("input[type=file]").val();
-    if ($(".plus-info.add-mobile-photo .photo-counter").text()=='3'){
-        $(".add-photos .fa-plus-square-o").show();
-        // alert(1);
-    }
+    var colPhotos = -1;
+
     if (img!='' && img!=undefined){
-        var date = new Date();
-        if ($(".plus-info.add-mobile-photo .photo-counter").text()=='3'){
-            // alert(3);
-            $(".add-photos .fa-plus-square-o").hide();
-        }else{
-            $(".add-photos .fa-plus-square-o").show();
-        }
-        $(".add-photos .clone").clone(true).insertBefore(".add-photos .plus").removeClass("clone").children().addClass('newPhoto');
+
+        $(".add-photos .col-lg-3").each(function (index) {
+            colPhotos++;
+        });
+
         img = img.replace("C:\\fakepath\\","");
         img = img.replace(/[\s]+/g, '');
         img = img.translit();
@@ -77,12 +72,15 @@ function add_photos() {
             cache: false,
             data: formData,
             success: function (data) {
-                $(".add-photos").children().each(function (index) {
-                    $(".photo-counter").parent().show().children(".photo-counter").text(index-2)
-                });
+
                 if(photos.indexOf(img) + 1) {
                     $(".newPhoto").parent().remove();
+                    $(".plus-info.add-mobile-photo .photo-counter").text(colPhotos-1);
                 } else{
+                    $(".add-photos .clone").clone(true).insertBefore(".add-photos .plus").removeClass("clone").children().addClass('newPhoto');
+                    $(".add-photos").children().each(function (index) {
+                        $(".photo-counter").parent().show().children(".photo-counter").text(colPhotos);
+                    });
                     $(".add-photos .newPhoto").css({
                         background :'url('+ img +')',
                         backgroundSize: 'cover',
@@ -90,6 +88,11 @@ function add_photos() {
                     }).removeClass('newPhoto');
                     photos.push(img);
                     $(".add-photo-fio .clone-hid").attr('value',photos);
+                }
+                if ($(".plus-info.add-mobile-photo .photo-counter").text()=='4'){
+                    $(".add-photos .fa-plus-square-o").hide();
+                }else{
+                    $(".add-photos .fa-plus-square-o").show();
                 }
                 $('#add-photo-file').parent().html($('#add-photo-file').parent().html());
             }
@@ -205,6 +208,7 @@ $(".cabinet .fa-trash, .cabinet #lk-photos .delete-photo").click(function () {
 
 
 $(".add-photos .delete-photo").click(function () {
+    $(".add-photos .fa-plus-square-o").show();
     var delPhoto= 'т';
     delPhoto = String($(this).parents(".photo").css('background-image'));
     delPhoto = delPhoto.replace('url("http://localhost/www/ProjectX/', '');
--- js/top.js
+++ js/top.js
@@ -8,6 +8,7 @@ function sel_top() {
         dataType: "json",
         success: function (data){
             // alert(data[0].main_photo);
+
             var slideTo = 1;
             var kolPhoto = 0;
             $(".fulltop #fulltop .col-md-4.clone").clone(true).show().insertBefore("#fulltop .plus")
@@ -15,6 +16,8 @@ function sel_top() {
                 .find("ol li").attr('data-target', '#carousel_01');
             for(var i=0;i<data.length;i++){
 //                    if (kolPhoto==2) return false;
+                if (data[i].photo == data[0].main_photo)
+                    var mainPhoto = data[i].photo;
                 var prevId = $("#fulltop .plus").prev().find(".item:last input[type=hidden]").val();
                 if (prevId == data[i].id_user){
                     $("#fulltop .plus").prev().find(".item.clone").clone(true)
@@ -74,36 +77,45 @@ function sel_top() {
             }
             $("#fulltop").find(".col-md-4.clone").next().remove();
 //--------------Вывод главной фотки v
-            $("#fulltop .plus").prev().find(".item.clone").clone(true)
-                .insertAfter($("#fulltop .plus").prev().find(".item:first")).removeClass("clone")
-                .children().css({
-                background: 'url('+ data[0].main_photo +')',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center 25%'
-            });
-            $("#fulltop .plus").prev().find(".item").each(function () {
-                $(this).removeClass("active");
-                $("#fulltop .plus").prev().find(".item:not(.clone):last").addClass("active");
-            });
-            $("#fulltop .plus").prev().find("ol li").each(function () {
-                $(this).removeClass("active");
-                $("#fulltop .plus").prev().find("ol li:not(.clone):last-child").addClass("active");
-            });
-            var eqPhoto =[];
-            $("#fulltop .plus").prev().find(".item:not(.clone)").each(function (index) {
-                var backImg= $(this).children().css('background-image');
-                if (eqPhoto.indexOf(backImg) + 1){
-                    $("#fulltop .plus").prev().find(".item:not(.clone)").each(function () {
-                        if ($(this).children().css('background-image') == backImg){
-                            $(this).remove();
-                            return false;
-                        }
+            if (mainPhoto != undefined) {
+                    $("#fulltop .plus").prev().find(".item.clone").clone(true)
+                        .insertAfter($("#fulltop .plus").prev().find(".item:first")).removeClass("clone")
+                        .children().css({
+                        background: 'url('+ mainPhoto +')',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center 25%'
                     });
-                }
-                else{
-                    eqPhoto.push(backImg);
-                }
-            });
+                $("#fulltop .plus").prev().find(".item.clone").clone(true)
+                    .insertAfter($("#fulltop .plus").prev().find(".item:first")).removeClass("clone")
+                    .children().css({
+                    background: 'url('+ data[0].main_photo +')',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center 25%'
+                });
+                $("#fulltop .plus").prev().find(".item").each(function () {
+                    $(this).removeClass("active");
+                    $("#fulltop .plus").prev().find(".item:not(.clone):last").addClass("active");
+                });
+                $("#fulltop .plus").prev().find("ol li").each(function () {
+                    $(this).removeClass("active");
+                    $("#fulltop .plus").prev().find("ol li:not(.clone):last-child").addClass("active");
+                });
+                var eqPhoto =[];
+                $("#fulltop .plus").prev().find(".item:not(.clone)").each(function (index) {
+                    var backImg= $(this).children().css('background-image');
+                    if (eqPhoto.indexOf(backImg) + 1){
+                        $("#fulltop .plus").prev().find(".item:not(.clone)").each(function () {
+                            if ($(this).children().css('background-image') == backImg){
+                                $(this).remove();
+                                return false;
+                            }
+                        });
+                    }
+                    else{
+                        eqPhoto.push(backImg);
+                    }
+                });
+            }
 //--------------Вывод главной фотки ^
             $("#fulltop .col-md-4.clone").next().remove();
             return false;
@@ -119,6 +131,7 @@ function firstTop() {
         dataType: "json",
         success: function (data){
             // alert(data[0].id_user);
+
 
             var slideTo = 1;
             $("#firsttop").find(".item.clone").clone(true)
@@ -145,6 +158,8 @@ function firstTop() {
             else
                 $("#firsttop").find(".about-vk").attr('href','https://vk.com/'+data[0].vk);
             for(var i=1;i<data.length;i++) {
+                if (data[i].photo == data[0].main_photo)
+                    var mainPhoto = data[i].photo;
                 var prevId = $("#firsttop .carousel-inner").find(".item input[type=hidden]:last-child").val();
 
                 if (prevId == data[i].id_user) {
@@ -183,36 +198,39 @@ function firstTop() {
                 }
 
             }
-            $("#firsttop").find(".item.clone").clone(true)
-                .appendTo($("#firsttop .carousel-inner")).removeClass("clone")
-                .children(".image").css({
-                background: 'url('+ data[0].main_photo +')',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center 25%'
-            });
-            $("#firsttop").find(".item").each(function () {
-                $(this).removeClass("active");
-                $("#firsttop").find(".item:not(.clone):last").addClass("active");
-            });
-            $("#firsttop").find("ol li").each(function () {
-                $(this).removeClass("active");
-                $("#firsttop").find("ol li:not(.clone):last-child").addClass("active");
-            });
-            var eqPhoto =[];
-            $("#firsttop").find(".item:not(.clone)").each(function (index) {
-                var backImg= $(this).children().css('background-image');
-                if (eqPhoto.indexOf(backImg) + 1){
-                    $("#firsttop").find(".item:not(.clone)").each(function () {
-                        if ($(this).children().css('background-image') == backImg){
-                            $(this).remove();
-                            return false;
-                        }
-                    });
-                }
-                else{
-                    eqPhoto.push(backImg);
-                }
-            });
+
+            if (mainPhoto != undefined) {
+                $("#firsttop").find(".item.clone").clone(true)
+                    .appendTo($("#firsttop .carousel-inner")).removeClass("clone")
+                    .children(".image").css({
+                    background: 'url(' + mainPhoto + ')',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center 25%'
+                });
+                $("#firsttop").find(".item").each(function () {
+                    $(this).removeClass("active");
+                    $("#firsttop").find(".item:not(.clone):last").addClass("active");
+                });
+                $("#firsttop").find("ol li").each(function () {
+                    $(this).removeClass("active");
+                    $("#firsttop").find("ol li:not(.clone):last-child").addClass("active");
+                });
+                var eqPhoto = [];
+                $("#firsttop").find(".item:not(.clone)").each(function (index) {
+                    var backImg = $(this).children().css('background-image');
+                    if (eqPhoto.indexOf(backImg) + 1) {
+                        $("#firsttop").find(".item:not(.clone)").each(function () {
+                            if ($(this).children().css('background-image') == backImg) {
+                                $(this).remove();
+                                return false;
+                            }
+                        });
+                    }
+                    else {
+                        eqPhoto.push(backImg);
+                    }
+                });
+            }
             return false;
 
         }
